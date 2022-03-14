ed065e221c0764f6b476337ff5a5b11929cb3411
Juan Cruz Soto <juancruzlp2013@gmail.com>
2022-03-14 14:20:22 -0300

    consults to firestore working

--- config-firebase.js
+++ config-firebase.js
@@ -1,18 +1,21 @@
-const firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/firestore');
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDIs4OeScVwrZvrwFlueKMX3YD-U5XvJ-k",
-    authDomain: "observatoriobovino-bbcb9.firebaseapp.com",
-    projectId: "observatoriobovino-bbcb9",
-    storageBucket: "observatoriobovino-bbcb9.appspot.com",
-    messagingSenderId: "44128638428",
-    appId: "1:44128638428:web:4446f49b72de1068ccab19",
-    measurementId: "G-7J3XN29J8T"
+  apiKey: "AIzaSyBLPic1kXD-AxUXDQZHqLADMtPWwxNFEp0",
+  authDomain: "calendar-bd-71d86.firebaseapp.com",
+  projectId: "calendar-bd-71d86",
+  storageBucket: "calendar-bd-71d86.appspot.com",
+  messagingSenderId: "702803305481",
+  appId: "1:702803305481:web:330ded7e57bc7e4358c4d8",
+  measurementId: "G-583X0XPZ3R",
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+const db = firebase.firestore();
+const auth = firebase.auth;
 
-module.exports = firebase;
+export { firebase, db,auth };
--- main.js
+++ main.js
@@ -1,28 +1,51 @@
-const { Telegraf } = require("telegraf");
-const { firebase } = require("./config-firebase")
-require('dotenv').config({path: ".env"});
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLPic1kXD-AxUXDQZHqLADMtPWwxNFEp0",
+  authDomain: "calendar-bd-71d86.firebaseapp.com",
+  projectId: "calendar-bd-71d86",
+  storageBucket: "calendar-bd-71d86.appspot.com",
+  messagingSenderId: "702803305481",
+  appId: "1:702803305481:web:330ded7e57bc7e4358c4d8",
+  measurementId: "G-583X0XPZ3R",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth;
+
+import { Telegraf } from "telegraf";
+// import {firebase} from
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
 
 const bot = new Telegraf(process.env.TOKEN_TELEGRAM);
 
 // console.log(db)
 
 const loadData = async (uid) => {
-    const response = await firebase.firestore().collection(`${uid}/cumpleaños/personas`).get();
-    const data = [];
+  const response = await db.collection(`${uid}/cumpleaños/personas`).get();
+  const data = [];
 
-    response.forEach((persona) => {
-      const personaData = persona.data();
+  response.forEach((persona) => {
+    const personaData = persona.data();
 
-      data.push({
-        id: persona.id,
-        ...personaData,
-      });
+    data.push({
+      id: persona.id,
+      ...personaData,
     });
+  });
 
-    return data;
-  };
+  console.log(data);
+  return data;
+};
 
-// console.log(loadData("gFx7y0skAHW4JvVaa51oyTY1wBe2"))
+console.log(loadData("gFx7y0skAHW4JvVaa51oyTY1wBe2"));
 
 let reminder;
 
@@ -30,14 +53,17 @@ const helpMessage =
   "Listado de comandos disponibles anashei \n /start \n /reminderON \n /reminderOff";
 
 bot.start((ctx) => {
-  ctx.reply("/help Hola");
-});
+  ctx.reply(
+    "Hola, para iniciar ingresa /login y sigue las instrucciones: \n /help - Visualizar lista de comandos"
+  );
+  console.log(ctx);
+});   
 
 bot.hears("/reminderON", (ctx) => {
   if (reminder) {
-    ctx.reply("Recordatorios ya se encuentran activados");}
-    else{
-        ctx.reply("Recordatorios Activados")
+    ctx.reply("Recordatorios ya se encuentran activados");
+  } else {
+    ctx.reply("Recordatorios Activados");
     reminder = setInterval(() => {
       console.log(new Date().getSeconds(), new Date().getHours());
       ctx.reply("aaaa");
@@ -48,12 +74,39 @@ bot.hears("/reminderON", (ctx) => {
 bot.hears("/reminderOff", (ctx) => {
   ctx.reply("Recordatorios Desactivados");
   clearInterval(reminder);
-  reminder = None
+  reminder = None;
 });
 
 bot.help((ctx) => {
   ctx.reply(helpMessage);
 });
-bot.hears("hi", (ctx) => ctx.reply("bit4you.io is awesome"));
+
+const regex = new RegExp("^(/connect_[a-zA-Z0-9_]*)$");
+
+// Math.random().toString(36).substr(2)
+bot.hears(regex, (ctx) => {
+  let data = ctx.message.text.split("_");
+  console.log(data);
+  db.collection(`${data[1]}/cumpleaños/telegram`)
+    .get()
+    .then((response) => {
+      response.forEach(async (persona) => {
+        if (persona.data().token === data[2]) {
+          ctx.reply("Cuenta enlazada con exito");
+          let response = await db
+            .collection(`${data[1]}/cumpleaños/profile`)
+            .get();
+          response.forEach((persona) => {
+            console.log(ctx.message.chat)
+            db.doc(`${data[1]}/cumpleaños/profile/${persona.id}`).update({...persona.data(),
+              messageid: ctx.message.chat.id
+            });
+          });
+        } else {
+          ctx.reply("No se pudo enlazar la cuenta, reingrese el comando");
+        }
+      });
+    });
+});
 
 bot.launch();
