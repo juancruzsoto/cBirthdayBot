85b03e5e23552e4c5988e9b93fdb7b2106d10905
Juan Cruz Soto <juancruzlp2013@gmail.com>
2022-03-15 14:14:32 -0300

    reminders and link account working

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLPic1kXD-AxUXDQZHqLADMtPWwxNFEp0",
@@ -17,10 +19,6 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const auth = firebase.auth;
-
-import { Telegraf } from "telegraf";
-// import {firebase} from
-import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
@@ -40,14 +38,43 @@ const loadData = async (uid) => {
       ...personaData,
     });
   });
-
-  console.log(data);
-  return data;
+  let currentYear = new Date().getFullYear() - 1;
+  let event = [];
+  for (var i = 0; i < 3; i++) {
+    // eslint-disable-next-line
+    data.map((person) => {
+      if (person.fecha.seconds) {
+        var t = new Date(1970, 0, 1); // Epoch
+        t.setSeconds(person.fecha.seconds);
+        t.setYear(currentYear);
+      } else {
+        t = person.fecha;
+        t.setYear(currentYear);
+      }
+      event.push({
+        id: person.id,
+        fecha: t.toISOString().slice(0, -14),
+        nombre: person.nombre,
+      });
+    });
+    currentYear++;
+  }
+  event.sort((a, b) => {
+    let fa = a.fecha.split("-"),
+      fb = b.fecha.split("-");
+    if (
+      new Date(+fa[0], fa[1] - 1, +fa[2]) > new Date(+fb[0], fb[1] - 1, +fb[2])
+    ) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  // console.log(event);
+  return event;
 };
 
-console.log(loadData("gFx7y0skAHW4JvVaa51oyTY1wBe2"));
-
-let reminder;
+// console.log(await loadData("gFx7y0skAHW4JvVaa51oyTY1wBe2"), "Asdasdassaddsa");
 
 const helpMessage =
   "Listado de comandos disponibles anashei \n /start \n /reminderON \n /reminderOff";
@@ -57,24 +84,82 @@ bot.start((ctx) => {
     "Hola, para iniciar ingresa /login y sigue las instrucciones: \n /help - Visualizar lista de comandos"
   );
   console.log(ctx);
-});   
+});
 
-bot.hears("/reminderON", (ctx) => {
-  if (reminder) {
-    ctx.reply("Recordatorios ya se encuentran activados");
+console.log(
+  new Date(
+    new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0, 0, 0, 0)
+  ).getTime() - new Date().getTime()
+);
+
+const recordatorio = (tiempo, ctx) => {
+  setTimeout(async () => {
+    let reminder = await db.doc(`usersTelegram/${ctx.message.chat.id}`).get();
+
+    if (reminder.data()) {
+      if (reminder.data().recordatorio === 1) {
+        let bdays = await loadData(reminder.data().token);
+        let aux = new Date();
+        bdays.forEach((bd, index, arr) => {
+          let fa = bd.fecha.split("-");
+          fa = new Date(+fa[0], fa[1] - 1, +fa[2]);
+          if (
+            fa.toISOString().slice(0, -14) === aux.toISOString().slice(0, -14)
+          ) {
+            ctx.reply(`HOY ES CUMPLEAÑOS DE: ${bd.nombre}`);
+            arr.length = index + 1;
+          }else{ctx.reply("prueba")}
+          if (fa > aux) {
+            arr.length = index + 1;
+          }
+        });
+        recordatorio(86400000, ctx);
+      }
+    }
+  }, tiempo);
+};
+
+bot.hears("/reminderON", async (ctx) => {
+  let reminder = await db.doc(`usersTelegram/${ctx.message.chat.id}`).get();
+
+  if (reminder.data()) {
+    if (reminder.data().recordatorio === 1) {
+      ctx.reply("Recordatorios ya se encuentran activados");
+    } else {
+      ctx.reply("Recordatorios Activados");
+      db.doc(`usersTelegram/${ctx.message.chat.id}`).set({
+        ...reminder.data(),
+        recordatorio: 1,
+      });
+      recordatorio(
+        new Date(
+          new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0,0,0,0)).getTime() - new Date().getTime(),
+        ctx
+      );
+    }
   } else {
-    ctx.reply("Recordatorios Activados");
-    reminder = setInterval(() => {
-      console.log(new Date().getSeconds(), new Date().getHours());
-      ctx.reply("aaaa");
-    }, 1000);
+    ctx.reply(
+      "Primero debe enlazar su cuenta con el bot, siga las instrucciones: https://cbirthday.herokuapp.com/telegram"
+    );
   }
 });
 
-bot.hears("/reminderOff", (ctx) => {
-  ctx.reply("Recordatorios Desactivados");
-  clearInterval(reminder);
-  reminder = None;
+bot.hears("/reminderOff", async (ctx) => {
+  let reminder = await db.doc(`usersTelegram/${ctx.message.chat.id}`).get();
+  if (reminder.data()) {
+    if (reminder.data().recordatorio === 0) {
+      ctx.reply("Recordatorios ya se encuentran desactivados");
+    } else {
+    db.doc(`usersTelegram/${ctx.message.chat.id}`).set({
+      ...reminder.data(),
+      recordatorio: 0,
+    });
+    ctx.reply("Recordatorios Desactivados")}
+  } else {
+    ctx.reply(
+      "Primero debe enlazar su cuenta con el bot, siga las instrucciones: https://cbirthday.herokuapp.com/telegram"
+    );
+  }
 });
 
 bot.help((ctx) => {
@@ -84,29 +169,34 @@ bot.help((ctx) => {
 const regex = new RegExp("^(/connect_[a-zA-Z0-9_]*)$");
 
 // Math.random().toString(36).substr(2)
-bot.hears(regex, (ctx) => {
-  let data = ctx.message.text.split("_");
-  console.log(data);
-  db.collection(`${data[1]}/cumpleaños/telegram`)
-    .get()
-    .then((response) => {
-      response.forEach(async (persona) => {
-        if (persona.data().token === data[2]) {
-          ctx.reply("Cuenta enlazada con exito");
-          let response = await db
-            .collection(`${data[1]}/cumpleaños/profile`)
-            .get();
-          response.forEach((persona) => {
-            console.log(ctx.message.chat)
-            db.doc(`${data[1]}/cumpleaños/profile/${persona.id}`).update({...persona.data(),
-              messageid: ctx.message.chat.id
+bot.hears(regex, async (ctx) => {
+  let reminder = await db
+    .doc(`usersTelegram/${ctx.message.chat.id1515234}`)
+    .get();
+  if (reminder.data()) {
+    let data = ctx.message.text.split("_");
+    console.log(data);
+    db.collection(`${data[1]}/cumpleaños/telegram`)
+      .get()
+      .then((response) => {
+        response.forEach(async (persona) => {
+          if (persona.data().token === data[2]) {
+            db.doc(`usersTelegram/${ctx.message.chat.id}`).set({
+              token: data[1],
+              recordatorio: 0,
             });
-          });
-        } else {
-          ctx.reply("No se pudo enlazar la cuenta, reingrese el comando");
-        }
+            ctx.reply("Cuenta enlazada con exito");
+          } else {
+            ctx.reply("No se pudo enlazar la cuenta, reingrese el comando");
+          }
+        });
+      })
+      .catch(() => {
+        ctx.reply("No se pudo enlazar la cuenta, reingrese el comando");
       });
-    });
+  } else {
+    ctx.reply("Su usuario ya se encuentra enlazado con una cuenta.");
+  }
 });
 
 bot.launch();
