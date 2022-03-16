638894d43b55f184af9456eb5fc69d5f1de8d2d2
Juan Cruz Soto <juancruzlp2013@gmail.com>
2022-03-16 11:08:20 -0300

    express

--- main.js
+++ main.js
@@ -3,6 +3,30 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import { Telegraf } from "telegraf";
 import dotenv from "dotenv";
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo desde Express.');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}...`);
+})
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLPic1kXD-AxUXDQZHqLADMtPWwxNFEp0",
+  authDomain: "calendar-bd-71d86.firebaseapp.com",
+  projectId: "calendar-bd-71d86",
+  storageBucket: "calendar-bd-71d86.appspot.com",
+  messagingSenderId: "702803305481",
+  appId: "1:702803305481:web:330ded7e57bc7e4358c4d8",
+  measurementId: "G-583X0XPZ3R",
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
